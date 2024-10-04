c158a8f1c7c7d07de5061b437744f214adc60b13
PabloHoracioVelazquezTelle <pablotz1504@gmail.com>
2024-10-04 02:32:49 -0600

    programa inicial v2

--- introOOP.py
+++ introOOP.py
@@ -1,15 +1,14 @@
 class Persona:
-    def __init__(self, nombre, edad, genero, ocupacion,oN): #<-------------Esto es un constructor
+    def __init__(self, nombre, edad, genero, ocupacion): #<-------------Esto es un constructor
         # Se definen con self.Atributos de la clase Persona
         self.nombre = nombre
         self.edad = edad
         self.genero = genero
         self.ocupacion = ocupacion
-        self.oN
+
     # Método para saludar
     def saludar(self):
         print(f"Hola, mi nombre es {self.nombre}.")
-        #return f"Hola, mi nombre es {self.nombre}."
 
     # Método para mostrar la edad
     def mostrar_edad(self):
@@ -32,24 +31,25 @@ class Persona:
             print(f"{self.nombre} no es mayor de edad.")
 
     #Mandar saludo
-    def mandar_saludo(oN):
-        print(f"Hola {self.nombre}, te manda a saludar,{self.oN}")
+    def mandar_saludo(self, otra_persona):
+        print(f"Hola {self.nombre}, te manda un saludo {otra_persona.nombre}.")
+
 
 # Crear una instancia de la clase Persona
 persona1 = Persona("Carlos", 25, "masculino", "ingeniero")
 persona2 = Persona("Juan", 19, "masculino", "Estudiante")
-#persona3 = Persona("Victor", 10, "masculino", "Docente")
-#persona4 = Persona("Brenda", 25, "femenino", "Secretaria")
+persona3 = Persona("Victor", 10, "masculino", "Docente")
+persona4 = Persona("Brenda", 25, "femenino", "Secretaria")
 # Usar los métodos de la clase
 persona1.saludar( )               # "Hola, mi nombre es Carlos."
 persona1.mostrar_edad()           # "Tengo 25 años."
 persona1.presentarse()            # "Soy Carlos, tengo 25 años, soy masculino y trabajo como ingeniero."
 persona1.cambiar_ocupacion("profesor")  # "Mi nueva ocupación es profesor."
 persona1.es_mayor_de_edad() # "Carlos es mayor de edad."
-print(persona1.mandar_saludo(persona2.nombre))
+persona1.mandar_saludo(persona3)
 #las funciones dentro de una clase se conocen como metodos
 print("\n")
-"""persona2.saludar()
+persona2.saludar()
 persona2.mostrar_edad()
 persona2.presentarse()
 persona2.cambiar_ocupacion("profesor")
@@ -65,4 +65,4 @@ persona4.saludar()
 persona4.mostrar_edad()
 persona4.presentarse()
 persona4.cambiar_ocupacion("profesor")
-persona4.es_mayor_de_edad()"""
+persona4.es_mayor_de_edad()
